e15f84986d52f7b6b73d0eb53d84a8ea0cb1a4ee
LiuYan-xwx <LiuYan_xwx@outlook.com>
2025-05-31 01:29:32 +0800

    feat: 添加 MonitorOption 转换器，组件设置的监控项目使用绑定
    在 `MonitorComponentSettingsControl.xaml` 中，新增命名空间和 `ObjectDataProvider`，为 `ComboBox` 提供 `MonitorOption` 枚举值，并更新绑定以支持双向数据绑定。同时，添加数据模板以使用 `MonitorOptionToStringConverter` 进行字符串转换。
    
    在 `MonitorOptionToStringConverter.cs` 中，新增 `MonitorOptionToStringConverter` 类，实现 `IValueConverter` 接口，用于将 `MonitorOption` 枚举值转换为字符串，并提供 `ConvertBack` 方法的基本实现。

---
+++ Converters/MonitorOptionToStringConverter.cs
@@ -0,0 +1,38 @@
+﻿using MonitorIsland.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MonitorIsland.Converters
+{
+    public class MonitorOptionToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is MonitorOption option)
+            {
+                return option switch
+                {
+                    MonitorOption.MemoryUsage => "内存使用量",
+                    MonitorOption.CpuUsage => "CPU 利用率",
+                    MonitorOption.CpuTemperature => "CPU 温度",
+                    _ => value.ToString() ?? string.Empty // 作为备选方案，显示枚举的原始名称
+                };
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // ComboBox 的 SelectedItem 直接绑定到枚举类型时，
+            // WPF 通常能处理好从选中项到枚举值的转换，这个方法可能不需要复杂实现。
+            // 如果遇到问题，可能需要根据 ComboBox 的具体绑定方式来调整。
+            // 对于当前 ItemSource 是枚举值，SelectedItem 也是枚举值的情况，此方法一般不会被调用或依赖。
+            throw new NotSupportedException("ConvertBack is not supported for MonitorOptionToStringConverter.");
+        }
+    }
+}
