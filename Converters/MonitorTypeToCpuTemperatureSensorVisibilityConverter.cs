caec72f46e9e37698c72962cd01d35f10b8b6fd6
LiuYan-xwx <LiuYan_xwx@outlook.com>
2025-07-14 21:42:19 +0800

    chore: 更新方法参数类型并移除注释
    在 `MonitorTypeToCpuTemperatureSensorVisibilityConverter.cs` 文件中，`Convert` 和 `ConvertBack` 方法的参数类型从可空类型（`object?`）更改为非可空类型（`object`），不再接受 `null` 值作为参数。同时，删除了方法前的 XML 注释。

--- Converters/MonitorTypeToCpuTemperatureSensorVisibilityConverter.cs
+++ Converters/MonitorTypeToCpuTemperatureSensorVisibilityConverter.cs
@@ -5,12 +5,9 @@ using System.Windows.Data;
 
 namespace MonitorIsland.Converters
 {
-    /// <summary>
-    /// 将监控类型转换为CPU温度传感器选择控件的可见性
-    /// </summary>
     public class MonitorTypeToCpuTemperatureSensorVisibilityConverter : IValueConverter
     {
-        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is MonitorOption monitorType)
             {
@@ -19,7 +16,7 @@ namespace MonitorIsland.Converters
             return Visibility.Collapsed;
         }
 
-        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
