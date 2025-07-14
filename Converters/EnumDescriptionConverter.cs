2b371d24009dbc9741a2b0e0ca2e778823493fd9
LiuYan-xwx <LiuYan_xwx@outlook.com>
2025-07-14 21:41:41 +0800

    feat: 替换监控选项转换器为枚举描述转换器
    在 `MonitorComponentSettingsControl.xaml` 中移除了 `MonitorOptionToStringConverter` 的引用，添加了 `EnumDescriptionConverter`，以便将枚举值转换为其描述属性的文本。相应地更新了 `ComboBox` 的 `ItemTemplate` 中的绑定。
    
    新增了 `EnumDescriptionConverter.cs` 文件，定义了 `EnumDescriptionConverter` 类，实现了 `IValueConverter` 接口。移除了不再使用的 `MonitorOptionToStringConverter.cs` 文件。

---
+++ Converters/EnumDescriptionConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+using System.Windows.Data;
+
+namespace MonitorIsland.Converters
+{
+    /// <summary>
+    /// 将枚举值转换为其Description属性的文本
+    /// </summary>
+    public class EnumDescriptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Enum enumValue)
+            {
+                var fieldInfo = enumValue.GetType().GetField(enumValue.ToString());
+                var descriptionAttribute = fieldInfo?.GetCustomAttribute<DescriptionAttribute>();
+                return descriptionAttribute?.Description ?? enumValue.ToString();
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("ConvertBack is not supported for EnumDescriptionConverter.");
+        }
+    }
+}
