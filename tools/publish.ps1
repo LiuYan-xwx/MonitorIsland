325efb14f0fcd8223caeaa11a432601decbec675
流焰xwx <66517348+LiuYan-xwx@users.noreply.github.com>
2026-02-28 15:40:06 +0800

    更新 publish.ps1

--- tools/publish.ps1
+++ tools/publish.ps1
@@ -1,4 +1,4 @@
-if (Test-Path -Path "./bin") {
-    Remove-Item ./bin -recurse
+if (Test-Path -Path "./MonitorIsland/bin") {
+    Remove-Item ./MonitorIsland/bin -recurse
 }
 dotnet publish -c Release -p:CreateCipx=true
