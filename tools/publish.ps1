4cb18f702a1b49c7afd996c69a38883b580a0093
LiuYan_xwx <LiuYan_xwx@outlook.com>
2026-06-26 02:20:41 +0800

    ci: 修改发布脚本

--- tools/publish.ps1
+++ tools/publish.ps1
@@ -1,4 +1,20 @@
-if (Test-Path -Path "./MonitorIsland/bin") {
-    Remove-Item ./MonitorIsland/bin -recurse
+$cipxPath = "./MonitorIsland/cipx"
+$publishPath = "./MonitorIsland/bin/Release/net8.0/publish"
+
+dotnet publish ./MonitorIsland/MonitorIsland.csproj -c Release
+
+if (Test-Path -Path $cipxPath)
+{
+    Remove-Item $cipxPath -Recurse -Force
 }
-dotnet publish -c Release -p:CreateCipx=true
+
+New-Item -Path $cipxPath -ItemType Directory
+
+$compress = @{
+  Path = "$publishPath/*"
+  DestinationPath = "$cipxPath/MonitorIsland.cipx"
+  Force = $True
+}
+Compress-Archive @compress
+
+pwsh -ep bypass "./tools/generate-md5.ps1" $cipxPath
